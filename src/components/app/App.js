2859e1eb99a022130b721562853d0ceb6d35648d
Eduard <eduard.nabiulin@gmail.com>
2022-08-23 22:16:49 +0300

    feat: add application structure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,18 @@ import './app.scss';
 
 export default function App() {
 	return (
-		<div>Weather</div>
+		<div className='main__container'>Weather</div>
 	)
 }
+
+/*
+* My API Key 0849950658e40b8c7e7d1d38e770e8bf
+*
+* Call current weather data => https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+*
+* Coordinates by location name => https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+*
+* Russian description => add get-param lang=ru
+*
+* Figma => https://www.figma.com/file/nB5Xkp4nilYI6h4WteAIM6/Weather-app-design-(Community)?node-id=0%3A1
+* */
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './styles/index.css';
 import './styles/reset.css';
+import './styles/index.css';
 
 import App from './components/app/App';
 
